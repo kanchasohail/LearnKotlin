e49a300314bb01401d35843eee8ef8b16a59ec49
Md Sohail <kanchasohail@gmail.com>
2023-08-11 16:15:38 +0530

    profile screen top area designed

--- app/src/main/java/com/social/learnkotlin/view/profile_screen/ProfileScreenViewModel.kt
+++ app/src/main/java/com/social/learnkotlin/view/profile_screen/ProfileScreenViewModel.kt
@@ -1,6 +1,12 @@
 package com.social.learnkotlin.view.profile_screen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.social.learnkotlin.R
 
 class ProfileScreenViewModel : ViewModel() {
+    var profileImage by mutableStateOf(R.drawable.ic_blank_profile)
+
 }
