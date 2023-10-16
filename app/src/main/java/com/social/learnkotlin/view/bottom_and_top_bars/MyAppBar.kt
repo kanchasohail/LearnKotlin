d1016fbd255a35fb7cf86e5df2e293e937e045dc
Md Sohail <kanchasohail@gmail.com>
2023-10-16 19:40:29 +0530

    kotlin data types lesson adde

--- app/src/main/java/com/social/learnkotlin/view/bottom_and_top_bars/MyAppBar.kt
+++ app/src/main/java/com/social/learnkotlin/view/bottom_and_top_bars/MyAppBar.kt
@@ -13,14 +13,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
